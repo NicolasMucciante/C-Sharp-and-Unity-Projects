c63e28376d0b223b6d210c9d0f024bc9f24c69c3
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-07 18:01:16 -0700

    Created Simple Comparison Program

---
+++ Anonymous Income Comparison Program/Anonymous Income Comparison Program/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_Income_Comparison_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // PERSON 1
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1:");
+            Console.WriteLine("Please enter your hourly pay rate.");
+            float personOneHourly = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your hours worked per week.");
+            float personOneWorked = Convert.ToInt32(Console.ReadLine());
+
+
+            //PERSON 2
+            Console.WriteLine("Person 2:");
+            Console.WriteLine("Please enter your hourly pay rate.");
+            float personTwoHourly = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your hours worked per week.");
+            float personTwoWorked = Convert.ToInt32(Console.ReadLine());
+            float personOneAnnual = personOneHourly * personOneWorked * 52;
+            float personTwoAnnual = personTwoHourly * personTwoWorked * 52;
+
+            //COMPARISONS
+            Console.WriteLine("Annual salary of Person 1: " + personOneAnnual);
+            Console.WriteLine("Annual salary of Person 2: " + personTwoAnnual);
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            Console.WriteLine(personOneAnnual > personTwoAnnual);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Challenge/Challenge/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 200 / 3;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            //int roomTemp = 70;
+            //int currentTemp = 70;
+
+            //bool isWarm = currentTemp <= roomTemp;
+            //bool isWarm = currentTemp != roomTemp;
+
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            Console.WriteLine("Please enter a number between 1-100.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            int result = userNum * 50;
+            Console.WriteLine("Your number multiplied by 50 is: " + result);
+
+            Console.WriteLine("Please enter another number between 1-100.");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+            int result2 = userNum2 + 25;
+            Console.WriteLine("Your number plus 25 is: " + result2);
+
+            Console.WriteLine("Please enter another number between 1-100.");
+            float userNum3 = Convert.ToInt32(Console.ReadLine());
+            float result3 = (float)(userNum3 / 12.5);
+            Console.WriteLine("Your number divided by 12.5 is: " + result3);
+
+            Console.WriteLine("Please enter another number between 1-100.");
+            int userNum4 = Convert.ToInt32(Console.ReadLine());
+            bool result4 = userNum4 > 50;
+            Console.WriteLine("Your number is greater than 50: " + result4);
+
+            Console.WriteLine("Please enter another number between 1-100.");
+            int userNum5 = Convert.ToInt32(Console.ReadLine());
+            int result5 = userNum5 % 7;
+            Console.WriteLine("Your number divided by 7 has a remainder of: " + result5);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

12a1d45c4c8c54f76f9dbc63db0ab7e195a1a8de
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-15 21:33:32 -0700

    Created main method submission

---
+++ Main Method Submission/Main Method Submission/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNum = 10;
+            float userNumF = 5.0F;
+            string myString = "50";
+
+            Console.WriteLine(userNum + " doubled is: " + myClass.mathOperation(userNum));
+            Console.WriteLine(userNumF + " plus 5 is: " + myClass.mathOperation(userNumF));
+            Console.WriteLine(myString + " minus 10 is: " + myClass.mathOperation(myString));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Main Method Submission/Main Method Submission/myClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Submission
+{
+    class myClass
+    {
+        public static int mathOperation(int userNum)
+        {
+            int result = userNum * 2;
+            return result;
+        }
+
+        public static int mathOperation(float userNumF)
+        {
+            int result2 = (int)(userNumF + 5.0F);
+            return result2;
+        }
+
+        public static int mathOperation(string myString)
+        {
+            int myInt = Convert.ToInt32(myString) - 10;
+
+            return myInt;
+        }
+    }
+}

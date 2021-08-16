1b7a6671126a7ec9bba050c6239deea031aafc92
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-15 20:02:07 -0700

    Created calling methods assignment

---
+++ Calling Methods Assignment/Calling Methods Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());            
+
+            Console.WriteLine("Your number plus 5 is: " + myClass.addFive(userNum));
+            Console.WriteLine("Your number minus 10 is: " + myClass.sub10(userNum));
+            Console.WriteLine("Your number divided in half is: " + myClass.inHalf(userNum));
+
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
---
+++ Calling Methods Assignment/Calling Methods Assignment/myClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    class myClass
+    {
+        public static int addFive(int userNum)
+        {
+            int addResult = userNum + 5;
+            return addResult;
+        }
+
+        public static int sub10(int userNum)
+        {
+            int subResult = userNum - 10;
+            return subResult;
+        }
+
+        public static int inHalf(int userNum)
+        {
+            int half = userNum / 2;
+            return half;
+        }
+    }
+}

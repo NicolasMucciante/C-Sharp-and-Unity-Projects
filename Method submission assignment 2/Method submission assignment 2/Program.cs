b6d953d712767ffe441b532da3bf1384cc055056
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-16 20:43:22 -0700

    Created method submission assignment 2

---
+++ Method submission assignment 2/Method submission assignment 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_submission_assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass Class = new myClass();
+
+            Console.WriteLine("Enter a number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number (Enter 0 for default (5))"); //It makes us enter an int so 0 will trigger the default
+            int y = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("The two numbers you entered added together equal: " + myClass.Addition(x, y));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method submission assignment 2/Method submission assignment 2/myClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_submission_assignment_2
+{
+    class myClass
+    {
+        public static int Addition(int x, int y = 5)
+        {
+            if (y == 0)
+            {
+                y = 5;
+            }
+            int result = x + y;
+            return result;
+        }
+
+    }
+}

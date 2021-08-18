12035eeb61c7ad0d4679d33e05c12fccaa9c4669
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-17 18:48:24 -0700

    Created method class submission 3

---
+++ Method class submission 3/Method class submission 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_class_submission_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            myClass Class = new myClass();
+
+            Console.WriteLine("Enter a number to be multiplied by 2");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a number to be displyed on the screen");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            myClass.multiply(x, y);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Method class submission 3/Method class submission 3/myClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_class_submission_3
+{
+    public class myClass
+    {
+        public static void multiply(int x, int y)
+        {
+            int Mresult = x * 2;
+            Console.WriteLine(Mresult);
+            Console.WriteLine(y);
+        }
+    }
+}

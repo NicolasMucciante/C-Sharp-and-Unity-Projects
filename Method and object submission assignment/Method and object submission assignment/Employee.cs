991c49cee800657922c4a9de284742d3dee0e793
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-18 19:57:33 -0700

    Created method and object submission assignment

---
+++ Method and object submission assignment/Method and object submission assignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_and_object_submission_assignment
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Method and object submission assignment/Method and object submission assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_and_object_submission_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            Person.SayName(employee);
+
+            Console.ReadLine();
+        }
+    }
+}

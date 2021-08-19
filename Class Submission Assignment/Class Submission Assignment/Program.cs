b7dd0ee95533cc3b33aa2b91a6493f478b8476ed
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-18 17:38:55 -0700

    Created class submission assignment

---
+++ Class Submission Assignment/Class Submission Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y; //Declaring a variable and not giving it a value so we can use it in the output method
+
+            Console.WriteLine("Enter a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number divided in half is: ");
+            myClass.divide(userNum); //Divide method will take the user input and divide it in half
+
+            Console.WriteLine("This is an example of a out method...");
+            myClass.outMethod(out y); //Output method
+
+            Console.WriteLine("Enter a decimal number");
+            float userNum2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your decimal number divided in half is: ");
+            myClass.divide(userNum2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class Submission Assignment/Class Submission Assignment/myClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission_Assignment
+{
+    class myClass
+    {
+        public static void divide(int x) //Static method that will divide a user input in half
+        {        
+            int result = x / 2;
+            Console.WriteLine(result);
+        }
+
+        public static void outMethod(out int y) //Out method that takes a variable and multiplies it by 2
+        {
+            y = 10;
+            int result2 = y * 2;
+            Console.WriteLine(result2);
+        }
+
+        public static void divide(float x) //Method overloading for a float
+        {
+            float result3 = x / 2;
+            Console.WriteLine(result3);
+        }
+    }
+}

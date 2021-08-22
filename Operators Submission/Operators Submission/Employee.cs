9d459d5c16214592e411eb6ff32cab5b74ebe210
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-21 22:12:31 -0700

    Created operator submission

---
+++ Operators Submission/Operators Submission/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Submission
+{
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int id { get; set; }
+
+        public static bool operator == (Employee x, Employee y) //Overloads the == operator
+        {
+            if (x.id == y.id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator != (Employee x, Employee y) //This is required to overload the == operator 
+        {
+            if (x.id != y.id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Operators Submission/Operators Submission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { firstName = "Sample", lastName = "Employee1", id = 1 }; //Creating 2 employees to compare
+            Employee employee2 = new Employee() { firstName = "Sample", lastName = "Employee2", id = 2 };
+
+            if(employee1.id == employee2.id) 
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not equal");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

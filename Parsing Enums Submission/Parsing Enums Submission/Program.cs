feb8ca1a2dd7eff7c56c22145653a2e5a4b88e41
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-22 21:55:48 -0700

    Created parsing enums assignment

---
+++ Parsing Enums Submission/Parsing Enums Submission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Submission
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Days days = new Days();
+
+            Console.WriteLine("Enter the current day of the week.");
+            string userDay = Console.ReadLine();
+
+            try
+            {
+                Days.DaysOfTheWeek userEnumDay = (Days.DaysOfTheWeek)Enum.Parse(typeof(Days.DaysOfTheWeek), userDay);
+                Console.WriteLine("Your current day is: " + userEnumDay);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}

7eaedb1e60490db6908287177a7c39ec2eac41c2
NicolasMucciante <nicolasmucciante8@gmail.com>
2021-08-23 19:52:28 -0700

    Created lambda submission

---
+++ Lambda Submission/Lambda Submission/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Submission
+{
+    public struct Employee
+    {
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public int id { get; set; }        
+
+    }
+}
---
+++ Lambda Submission/Lambda Submission/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Submission
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeesList = new List<Employee> //Creating a new list of employees
+            {
+            new Employee() { fName = "Nick", lName = "Mucciante", id = 1 }, 
+            new Employee() { fName = "Joe", lName = "Thomas", id = 2 },
+            new Employee() { fName = "Matthew", lName = "Larson", id = 3 },
+            new Employee() { fName = "Spongebob", lName = "Squarepants", id = 4 },
+            new Employee() { fName = "Gary", lName = "Snail", id = 5},
+            new Employee() { fName = "Sandy", lName = "Cheeks", id = 6 },
+            new Employee() { fName = "Sheldon", lName = "Plankton", id = 7 },
+            new Employee() { fName = "Eugene", lName = "Crabs", id = 8 },
+            new Employee() { fName = "Squidward", lName = "Tentacles", id = 9 },
+            new Employee() { fName = "Joe", lName = "Smith", id = 10 }
+        };
+            List<Employee> JoeList = new List<Employee>(); //Creating a new list for employees named Joe
+
+            foreach (Employee employee in employeesList) //This loop parses the employeeList for employees named Joe and adds them to the JoeList
+            {
+                if(employee.fName == "Joe")
+                {
+                    JoeList.Add(employee);
+                }
+            }
+
+            List<Employee> JoeList2 = employeesList.Where(x => x.fName == "Joe").ToList(); //This does the same thing as the foreach loop
+
+            List<Employee> employees2 = employeesList.Where(x => x.id > 5).ToList(); //This will make a new list of every employee with an ID higher than 5
+
+
+            Console.ReadLine();
+        }
+    }
+}
